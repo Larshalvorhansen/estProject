ef316360db258b7f8641892ec4856a1918f20854
lhh <larshalvorhansen1@gmail.com>
2024-04-22 12:50:44 +0200

    continue fft code

--- fft.py
+++ fft.py
@@ -1,6 +1,8 @@
-import math
-import scipy
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def functionGen():
-    x(t)=
+def functionGenerator(t, A, w_0, phi, w_t):
+    return A * np.exp(1j*(w_0*t + phi)) + w_t
+
+def fft():
