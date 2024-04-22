c694c72cc1d187970bb367ffc1c035892581d269
lhh <larshalvorhansen1@gmail.com>
2024-04-22 14:17:17 +0200

    fix snrdB conversion

--- functionGen.py
+++ functionGen.py
@@ -5,18 +5,21 @@ from scipy import fft, ifft
 
 
 def functionGenerator(SNR):
+    dBinvSNR = math.pow(10,(SNR)/10)
     t = np.linspace(-0.000256, 0.000256, num=513)
     f_0 = 100000
     w_0 = 2*math.pi*f_0
     phi = math.pi/8
     sigma = 1/math.sqrt(2*SNR)
     A = 1
-    wn = np.random.normal(0, sigma)
+    wn = np.random.normal(0, sigma,t.shape)
     x = A * np.exp(1j*(w_0*t + phi)) + wn
-    return(x,A)
+    print(SNR)
+    print(dBinvSNR)
+    return(x,t)
 
-x = functionGenerator(1)
-plt.plot(x)
+x, t = functionGenerator(15)
+plt.plot(t,x)
 plt.title('x(t)')
 plt.xlabel('t')
 plt.ylabel('Amplitude')
