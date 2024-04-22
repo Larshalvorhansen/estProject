6ae63901495ca61a75c6d91000554f456748cb55
lhh <larshalvorhansen1@gmail.com>
2024-04-22 14:00:35 +0200

    change and rename fft.py to functionGen

--- functionGen.py
+++ functionGen.py
@@ -5,15 +5,17 @@ from scipy import fft, ifft
 
 
 def functionGenerator(SNR):
-    A = 1
-    t = np.linspace(0,5, num=513)
-    wn = np.random.normal(0, 0.5, t.shape)
+    t = np.linspace(-0.000256, 0.000256, num=513)
+    f_0 = 100000
+    w_0 = 2*math.pi*f_0
     phi = math.pi/8
-    x = A * np.exp(1j*(w_0*t + phi)) + SNR*wn
+    sigma = 1/math.sqrt(2*SNR)
+    A = 1
+    wn = np.random.normal(0, sigma)
+    x = A * np.exp(1j*(w_0*t + phi)) + wn
     return(x,A)
 
 x = functionGenerator(1)
-print(A)
 plt.plot(x)
 plt.title('x(t)')
 plt.xlabel('t')
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-import numpy as np
-import math
+# import numpy as np
+# import math
+# import matplotlib.pyplot as plt
+# from scipy import fft, ifft
+
+# def functionGenerator():
+#     t = np.linspace(0, 0.000513, num=513)
+#     return(t)
+
+# print(functionGenerator())
+
+
 import matplotlib.pyplot as plt
-from scipy import fft, ifft
-
-def functionGenerator():
-    t = np.linspace(0,5, num=513)
-    return(t)
-
-
-print(functionGenerator())
+count, bins, ignored = plt.hist(s, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+plt.show()
